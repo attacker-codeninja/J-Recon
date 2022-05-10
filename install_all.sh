f52e7774e17409e442801e3076509b20f2a9c70e
hack2gather <Jerry1319@protonmail.com>
2022-05-10 13:59:53 -0400

    Jerry

---
+++ install_all.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+
+echo "Installing all the dependencies and required materials for running the tool"
+# INSTALLING  RECON DEPENDENCIES
+bash install.sh 
+sleep 2 
+
+# INSTALLING THE CRAZIEST ONE 
+bash setup.sh
+sleep 2
+echo "If any tool got not install then plz install them manually for complete useage of the tool"
+sleep 2
+echo "Installation process for J-Recon tool was succeed"
